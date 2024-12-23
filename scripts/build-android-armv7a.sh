580a35f127db8d69712b408923bd66971b04bcc0
Miku25510 <filesvideo25510@gmail.com>
2024-12-23 22:19:24 +0700

    Update build-android-armv7a.sh

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -6,7 +6,11 @@ export ANDROID_NDK_HOME=$PWD/android-ndk-r10e/
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.1.0/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz -o /dev/null
 tar -xf clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
-./waf configure -T release --build-game=episodic --prefix=srceng-mod-launcher/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --32bits --enable-opus --togles --disable-warns &&
+# hl2 ->  Half-Life 2
+# episodic  ->  Half-Life 2 Episode One And Two
+# hl1       ->  Half Life: Source
+# portal    ->  Portal
+./waf configure -T release --build-game=hl2 --prefix=srceng-mod-launcher/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --32bits --enable-opus --togles --disable-warns &&
 ./waf install --target=client,server --strip
 
 if [ -e "srceng-mod-launcher/android/lib/armeabi-v7a/README.md" ]; then
