79dc2483ae282701accb225870a6ac45ae726295
Miku25510 <filesvideo25510@gmail.com>
2024-12-24 06:31:56 +0700

    Update build-android-armv7a.sh

--- scripts/build-android-armv7a.sh
+++ scripts/build-android-armv7a.sh
@@ -10,7 +10,7 @@ export PATH="$PWD/clang+llvm-11.1.0-x86_64-linux-gnu-ubuntu-16.04/bin:$PATH"
 # episodic  ->  Half-Life 2 Episode One And Two
 # hl1       ->  Half Life: Source
 # portal    ->  Portal
-./waf configure -T release --build-gameh=episodic --prefix=srceng-mod-launcher/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --32bits --enable-opus --togles --disable-warns &&
+./waf configure -T release --build-game=episodic --prefix=srceng-mod-launcher/android --android=armeabi-v7a-hard,host,21 --target=../armeabi-v7a --32bits --enable-opus --togles --disable-warns &&
 ./waf install --target=client,server --strip
 
 if [ -e "srceng-mod-launcher/android/lib/armeabi-v7a/README.md" ]; then
