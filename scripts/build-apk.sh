9d46eea251b7c1de22d1134bd161a984fc98b823
Miku25510 <filesvideo25510@gmail.com>
2024-12-24 07:58:03 +0700

    Update build-apk.sh

--- scripts/build-apk.sh
+++ scripts/build-apk.sh
@@ -5,8 +5,8 @@ cd srceng-mod-launcher
 git clone https://gitlab.com/LostGamer/android-sdk
 export ANDROID_SDK_HOME=$PWD/android-sdk
 git pull
-chmod +x android/script.sh
-./android/scripts/script.sh
+chmod +x android/scripts/script.sh
+android/scripts/script.sh
 chmod +x waf
 ./waf configure -T release &&
 ./waf build
