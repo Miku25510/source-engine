c2a4fcbb4bcc6d49323fb3c92d0e4653c9298a42
Miku25510 <filesvideo25510@gmail.com>
2024-12-24 10:26:11 +0700

    Update hl2_gamerules.cpp

--- game/shared/hl2/hl2_gamerules.cpp
+++ game/shared/hl2/hl2_gamerules.cpp
@@ -161,6 +161,10 @@ ConVar	sk_plr_dmg_smg1_grenade	( "sk_plr_dmg_smg1_grenade","0", FCVAR_REPLICATED
 ConVar	sk_npc_dmg_smg1_grenade	( "sk_npc_dmg_smg1_grenade","0", FCVAR_REPLICATED);
 ConVar	sk_max_smg1_grenade		( "sk_max_smg1_grenade","0", FCVAR_REPLICATED );
 
+ConVar  sk_plr_dmg_stickybomb        ("sk_plr_dmg_stickybomb", "0", FCVAR_REPLICATED);
+ConVar  sk_npc_dmg_stickybomb        ("sk_npc_dmg_stickybomb", "0", FCVAR_REPLICATED);
+ConVar  sk_max_stickybomb                ("sk_max_stickybomb", "0", FCVAR_REPLICATED);
+
 ConVar	sk_plr_dmg_357			( "sk_plr_dmg_357", "0", FCVAR_REPLICATED );
 ConVar	sk_npc_dmg_357			( "sk_npc_dmg_357", "0", FCVAR_REPLICATED );
 ConVar	sk_max_357				( "sk_max_357", "0", FCVAR_REPLICATED );
