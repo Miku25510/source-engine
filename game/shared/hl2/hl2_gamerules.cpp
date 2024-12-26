79344c7dba7b7f4d4a220a3ea952a734ae1a9581
Miku25510 <filesvideo25510@gmail.com>
2024-12-26 18:15:59 +0700

    Update hl2_gamerules.cpp

--- game/shared/hl2/hl2_gamerules.cpp
+++ game/shared/hl2/hl2_gamerules.cpp
@@ -107,6 +107,10 @@ ConVar	sk_npc_dmg_ar2			( "sk_npc_dmg_ar2","0", FCVAR_REPLICATED);
 ConVar	sk_max_ar2				( "sk_max_ar2","0", FCVAR_REPLICATED);
 ConVar	sk_max_ar2_altfire		( "sk_max_ar2_altfire","0", FCVAR_REPLICATED);
 
+ConVar	sk_plr_dmg_nailgun		("sk_plr_dmg_nailgun", "0", FCVAR_REPLICATED);
+ConVar	sk_npc_dmg_nailgun		("sk_npc_dmg_nailgun", "0", FCVAR_REPLICATED);
+ConVar	sk_max_nailgun			("sk_max_nailgun", "0", FCVAR_REPLICATED);
+
 ConVar	sk_plr_dmg_alyxgun		( "sk_plr_dmg_alyxgun","0", FCVAR_REPLICATED );
 ConVar	sk_npc_dmg_alyxgun		( "sk_npc_dmg_alyxgun","0", FCVAR_REPLICATED);
 ConVar	sk_max_alyxgun			( "sk_max_alyxgun","0", FCVAR_REPLICATED);
@@ -1804,6 +1808,7 @@ CAmmoDef *GetAmmoDef()
 
 		def.AddAmmoType("AR2",				DMG_BULLET,					TRACER_LINE_AND_WHIZ,	"sk_plr_dmg_ar2",			"sk_npc_dmg_ar2",			"sk_max_ar2",			BULLET_IMPULSE(200, 1225), 0 );
 		def.AddAmmoType("AlyxGun",			DMG_BULLET,					TRACER_LINE,			"sk_plr_dmg_alyxgun",		"sk_npc_dmg_alyxgun",		"sk_max_alyxgun",		BULLET_IMPULSE(200, 1225), 0 );
+		def.AddAmmoType("Nailgun",                      DMG_BULLET,					TRACER_LINE_AND_WHIZ,	"sk_plr_dmg_nailgun",		        "sk_npc_dmg_nailgun",		"sk_max_nailgun",		BULLET_IMPULSE(200, 1225), 0);
 		def.AddAmmoType("Pistol",			DMG_BULLET,					TRACER_LINE_AND_WHIZ,	"sk_plr_dmg_pistol",		"sk_npc_dmg_pistol",		"sk_max_pistol",		BULLET_IMPULSE(200, 1225), 0 );
 		def.AddAmmoType("SMG1",				DMG_BULLET,					TRACER_LINE_AND_WHIZ,	"sk_plr_dmg_smg1",			"sk_npc_dmg_smg1",			"sk_max_smg1",			BULLET_IMPULSE(200, 1225), 0 );
 		def.AddAmmoType("357",				DMG_BULLET,					TRACER_LINE_AND_WHIZ,	"sk_plr_dmg_357",			"sk_npc_dmg_357",			"sk_max_357",			BULLET_IMPULSE(800, 5000), 0 );
